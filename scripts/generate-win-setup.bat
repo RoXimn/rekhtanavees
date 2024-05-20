89afab7dd298fe8c0abf4241ba407d0b38c9b733
RoXimn <roximn@rixir.org>
2024-05-20 08:28:22 +0300

    updated setup gen script to build docs before setup file generation

--- scripts/generate-win-setup.bat
+++ scripts/generate-win-setup.bat
@@ -1,6 +1,10 @@
 @echo off
 set INNOSETUP_DIR="C:\Program Files (x86)\Inno Setup 6"
 
+cd docs
+call make html
+cd ..
+
 pyinstaller rekhtanavees.app.spec
 
 %INNOSETUP_DIR%\ISCC.exe installer\rekhtanavees-installer.iss
