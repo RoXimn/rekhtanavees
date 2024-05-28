8b8cc5b42d39e391a0d46464380ab778cb5c1c73
RoXimn <roximn@rixir.org>
2024-05-28 15:23:05 +0300

    Updated audio spectrum generation
    * Separated audio spectrum generation from QImage generation into different classes.
    * Added interval specific spectrum creation of the audio
    * Added superimposed word rendering onto the spectrum
    * time <-> pixel conversion funcs
    * viridis colormap from matplotlib

--- rekhtanavees/audio/audioclip.py
+++ rekhtanavees/audio/audioclip.py
@@ -8,13 +8,10 @@
 # Author:      RoXimn <roximn@rixir.org>
 # ******************************************************************************
 from pathlib import Path
-from typing import Any
 
 import librosa
 import numpy as np
-from PySide6.QtGui import QImage
-
-from rekhtanavees.audio.spectra import COLOR_MAPS_8BIT
+from numpy.typing import NDArray
 
 
 # ******************************************************************************
@@ -31,15 +28,30 @@ class AudioClip(object):
 
     # **************************************************************************
     def __init__(self):
-        self.audioSignal: np.ndarray = np.array([], dtype='float32')
-        self.sampleRate: int = 48000
+        self.audioSignal: NDArray = None
+        self.sampleRate: int = 0
+
+    # **************************************************************************
+    def __len__(self) -> int:
+        """Length of audio clip in milliseconds"""
+        return self.audioSignal.shape[0] // (self.sampleRate * 1000)
+
+    # **************************************************************************
+    def sample2time(self, sample: int) -> int:
+        """Get time(ms) of the given sample"""
+        return sample * 1000 // self.sampleRate
+
+    # **************************************************************************
+    def time2sample(self, time: int) -> int:
+        """Get the audio sample corresponding to the given time(milliseconds)"""
+        return time * self.sampleRate // 1000
 
     # **************************************************************************
     @staticmethod
     def createAudioClip(filePath: Path) -> 'AudioClip':
         """Create an audio clip object from the given audio file.
 
-        Uses librosa to load audio files, hence all formats supported by lbrosa can
+        Uses librosa to load audio files, hence all formats supported by librosa can
         be used.
 
         Args:
@@ -55,8 +67,7 @@ class AudioClip(object):
     # **************************************************************************
     def createSpectrogram(self, startTime: int = None, endTime: int = None,
                           melBins: int = 48, hopLength: int = 512,
-                          nFFT: int = 2048,
-                          vtickTime: int = 1, vtickValue: int = 192) -> np.ndarray[Any, np.dtype]:
+                          nFFT: int = 2048) -> tuple[NDArray[np.uint8], int, int]:
         """Create a Mel Spectrogram of the mono audio signal in the given time interval
 
         Note:
@@ -90,78 +101,41 @@ class AudioClip(object):
             hopLength (int): Interval between windows of Short FFT sampling
             nFFT (int): The window length of the SFFT sampling
             melBins (int): Number of bins in Mel Spectrogram (height of the spectrogram image)
-            vtickTime (int): Time interval to mark on the spectrogram in seconds. Use 0 to turn off
-            vtickValue (int): Pixel value to use for seconds tick mark in spectrogram, from the colormap
-            cmap (list[int]): 256 `AARRGGBB` pixel value colormap for the spectrogram
-        """
 
-        assert self.audioSignal.ndim == 1
-        LAST = self.audioSignal.shape[0] - 1
+        Returns:
+            (tuple[NDArray[np.uint8], int, int]): A 2D map of the db normalized
+                mel spectrogram, and its starting and ending time in milliseconds.
+        """
+        assert self.audioSignal is not None and isinstance(self.audioSignal, np.ndarray)
+        assert self.audioSignal.ndim == 1, f"Single channel audio required; [{self.audioSignal.ndim}] channel given"
+        LAST = self.audioSignal.shape[0]
         if LAST < 0:
-            return np.array([])
+            return np.array([]), 0, 0
 
-        startTime = 0 if startTime is None else startTime * self.sampleRate // 1000
-        endTime = LAST if endTime is None else endTime * self.sampleRate // 1000
-        startTime = np.clip(startTime, a_min=0, a_max=LAST)
-        endTime = np.clip(endTime, a_min=0, a_max=LAST)
-        if startTime > endTime:
-            startTime, endTime = endTime, startTime
+        firstSample = 0 if startTime is None else self.time2sample(startTime)
+        lastSample = LAST if endTime is None else self.time2sample(endTime)
+        firstSample = max(min(firstSample, LAST), 0)    # Clip to [0-LAST]
+        lastSample = max(min(lastSample, LAST), 0)        # Clip to [0-LAST]
+        if firstSample > lastSample:                     # Ensure lastSample > firstSample
+            firstSample, lastSample = lastSample, firstSample
+        elif firstSample == lastSample:
+            return np.array([]), firstSample, lastSample
 
-        melSpectrum = librosa.feature.melspectrogram(y=self.audioSignal[startTime:endTime],
-                                                     sr=self.sampleRate,
-                                                     n_mels=melBins,
-                                                     n_fft=nFFT,
-                                                     hop_length=hopLength)
-        dbMelSpectrum = librosa.amplitude_to_db(melSpectrum)
+        melSpectrum: NDArray = librosa.feature.melspectrogram(
+            y=self.audioSignal[firstSample:lastSample],
+            sr=self.sampleRate,
+            n_mels=melBins,
+            n_fft=nFFT,
+            hop_length=hopLength
+        )
+
+        dbMelSpectrum: NDArray = librosa.power_to_db(melSpectrum, ref=np.max)
         # Normalize and scale to 0..255 values
-        byteNormalizedSpectrum = (255 * (dbMelSpectrum - np.min(dbMelSpectrum)) / np.ptp(dbMelSpectrum)).astype('uint8')
+        byteNormalizedSpectrum: NDArray[np.uint8] = (255 * (dbMelSpectrum - np.min(dbMelSpectrum))
+                                                     / np.ptp(dbMelSpectrum)).astype('uint8')
         # flip vertically so low frequencies are at the bottom
-        byteMap = np.flip(byteNormalizedSpectrum, axis=0)
-        if vtickTime > 0:
-            # Add second ticks to the spectrogram image
-            imgHeight, imgWidth = byteMap.shape
-            frameRate = self.sampleRate // hopLength
-            vtickInterval = vtickTime * frameRate
-            secondStops = np.arange(start=vtickInterval, stop=imgWidth, step=vtickInterval)
-            vtickValue = vtickValue % 256
-            for xt in secondStops:
-                byteMap[:, xt] = vtickValue
-        return byteMap
+        byteMap: NDArray[np.uint8] = np.flip(byteNormalizedSpectrum, axis=0)
 
-    # **************************************************************************
-    def getSpectrumImage(self, startTime: int = None, endTime: int = None,
-                         widthPerSec: int = 24, height: int = 48,
-                         nFFT: int = 2048,
-                         timeMarker: int = 1, markerColor: int = 192,
-                         cmap: str = 'magma') -> QImage:
-        """Create Mel Spectrogram QImage of the recorded audio signal.
-
-        Args:
-            startTime (Optional[int]): start time interval (ms). Defaults to ``None``,
-                indicating beginning of the clip.
-            endTime (Optional[int]): end time interval (ms). Defaults to ``None``,
-                indicating end of the clip.
-            widthPerSec (int): Width of spectrogram image corresponding to one second
-            height (int): Height of spectrogram image
-            nFFT (int): Window length of Short FFT sampling of audio signal
-            timeMarker (int): Time marker placement interval, 0 to turn off.
-            markerColor (int): Pixel value to use for seconds tick mark in spectrogram, from the colormap
-            cmap (str): Colormap name to use for the spectrogram. Opt from ``magma``
-                and ``grayscale``. Add ``_r`` to name for reverse map.
-        """
-
-        byteMap = self.createSpectrogram(startTime=startTime, endTime=endTime,
-                                         melBins=height,
-                                         hopLength=self.sampleRate//widthPerSec,
-                                         nFFT=nFFT,
-                                         vtickTime=timeMarker,
-                                         vtickValue=markerColor)
-        imgHeight, imgWidth = byteMap.shape
-        imgSpectrum = QImage(np.ascontiguousarray(byteMap).data,
-                             imgWidth, imgHeight, imgWidth,
-                             QImage.Format_Indexed8)
-        imgSpectrum.setColorTable(COLOR_MAPS_8BIT.get(cmap, COLOR_MAPS_8BIT['magma']))
-        return imgSpectrum
-
+        return byteMap, self.sample2time(firstSample), self.sample2time(lastSample)
 
 # ******************************************************************************
---
+++ rekhtanavees/audio/clipimage.py
@@ -0,0 +1,228 @@
+# -*- coding: utf-8 -*-
+# ******************************************************************************
+# Copyright (c) 2024. All rights reserved.
+#
+# This work is licensed under the Creative Commons Attribution 4.0 International License.
+# To view a copy of this license, visit # http://creativecommons.org/licenses/by/4.0/.
+#
+# Author:      RoXimn <roximn@rixir.org>
+# ******************************************************************************
+import numpy as np
+from PySide6.QtCore import (Qt, QRectF, QPointF, QMargins)
+from PySide6.QtGui import (QImage, QPainter, QBrush, QFont, QTransform)
+from pydantic import BaseModel
+
+from rekhtanavees.audio.audioclip import AudioClip
+from rekhtanavees.audio.spectra import COLOR_MAPS_8BIT
+
+
+# **************************************************************************
+class Word(BaseModel):
+    start: float
+    end: float
+    word: str
+    probability: float
+
+
+# **************************************************************************
+class Segment(BaseModel):
+    id: int
+    start: float
+    end: float
+    text: str
+    avg_logprob: float
+    compression_ratio: float
+    no_speech_prob: float
+    words: list[Word]
+
+
+# **************************************************************************
+class ClipImage:
+    """Audio class representing an audio signal.
+
+    An audio signal wrapper class for convenient audio signal processing and
+    management.
+
+    Attributes:
+        audioClip (AudioClip): the audio clip this image represents
+        widthPerSec (Optional[int]): Width of spectrogram image corresponding to one second
+        height (Optional[int]): Height of spectrogram image
+        direction (Optional[Qt.LayoutDirection]): Defaults to Qt.LayoutDirection.LeftToRight
+        cmap (Optional[str]): Colormap name to use for the spectrogram. Opt from ``magma``
+            and ``grayscale``. Add ``_r`` to name for reverse map.
+    """
+    # **************************************************************************
+    def __init__(self,
+                 audioClip: AudioClip,
+                 widthPerSec: int = 24,
+                 height: int = 48,
+                 cmap: str = 'magma',
+                 direction: Qt.LayoutDirection = Qt.LayoutDirection.LeftToRight):
+
+        assert audioClip is not None
+        self.audioClip: AudioClip = audioClip
+        self.widthPerSec: int = widthPerSec
+        self.height: int = height
+        self.cmap: list[int] = COLOR_MAPS_8BIT.get(cmap, COLOR_MAPS_8BIT['magma'])
+        self.direction: Qt.LayoutDirection = direction
+
+    # **************************************************************************
+    def pixel2time(self, x: int, clipStart: int = 0) -> int:
+        """Get time(ms) of the given sample"""
+        return clipStart + x * 1000 // self.widthPerSec
+
+    # **************************************************************************
+    def time2pixel(self, t: int, clipStart: int = 0) -> int:
+        """Get the x coordinate corresponding to the given time(milliseconds)"""
+        return (t - clipStart) * self.widthPerSec // 1000
+
+    # **************************************************************************
+    def renderSpectrum(self, startTime: int = None, endTime: int = None, nFFT: int = 2048,
+                       markers: dict[int, list[int]] = None) -> QImage:
+        """Create Mel Spectrogram QImage of the given audio clip.
+
+        Args:
+            startTime (Optional[int]): indicating beginning of the clip (ms). Defaults to ``None``
+            endTime (Optional[int]): indicating end of the clip (ms). Defaults to ``None``
+            nFFT (Optional[int]): Window length of Short FFT sampling of audio signal
+            markers (Optional[dict[int, list[int]]]): a `dict` with color as key,
+                mapping to a list of time markers (in milliseconds).
+                For example, {192: [1000, 2000, 3000], 128: [50, 1120, 2455]},
+                different colored markers can indicate regular time interval,
+                word boundaries, confidence, probability, etc.
+
+        Note:
+            All times are global, i.e. relative to the beginning of the `audioClip`.
+        """
+
+        spectrum, start, end = self.audioClip.createSpectrogram(
+            startTime=startTime, endTime=endTime,
+            melBins=self.height,
+            hopLength=self.audioClip.sampleRate//self.widthPerSec,
+            nFFT=nFFT
+        )
+        duration: int = end - start
+        if spectrum.ndim == 2:
+            imgHeight, imgWidth = spectrum.shape
+            print(f'Spectrum {imgWidth}x{imgHeight}: {start}-{end}[{duration}ms]')
+
+            if markers is not None:
+                assert isinstance(markers, dict)
+                for c, ticks in markers.items():
+                    # print(c, ticks)
+                    tickValue = c % 256
+                    ticks: list[int] = [t for t in ticks if start <= t <= end]
+                    # print(tickValue, ticks)
+                    marks = [self.time2pixel(t, start) for t in ticks]
+                    # print(marks)
+                    for xt in marks:
+                        spectrum[:, xt] = tickValue
+
+            if self.direction == Qt.LayoutDirection.RightToLeft:
+                spectrum = np.flip(spectrum, axis=1)
+
+            image = QImage(np.ascontiguousarray(spectrum).data,
+                           imgWidth, imgHeight, imgWidth,
+                           QImage.Format_Indexed8)
+            image.setColorTable(self.cmap)
+        else:
+            image = QImage()
+
+        image = image.convertToFormat(QImage.Format_ARGB32)
+        image.startTime = start
+        return image
+
+    # **************************************************************************
+    def renderWords(self, image: QImage, label: str, words: list[Word]) -> QImage:
+        """Write the word at the given timeframe on the image with given marking.
+
+        Args:
+            image (QImage): image to render upon
+            label (str): segment label
+            words (Word): the word string to write
+
+        Note:
+            Needs the ``startTime`` attribute on the given ``image``.
+
+        ..TODO:
+            Add user rendering config
+        """
+        assert image.format() == QImage.Format_ARGB32 and hasattr(image, 'startTime')
+        if image.isNull():
+            return image
+
+        p = QPainter()
+        p.begin(image)
+        p.setRenderHint(QPainter.Antialiasing)
+
+        for w in words:
+            lt = self.time2pixel(int(w.start * 1000), image.startTime)
+            rt = self.time2pixel(int(w.end * 1000), image.startTime)
+            top = 0
+            btm = image.height() - 1
+
+            wordBox = QRectF(QPointF(lt, top), QPointF(rt, btm))
+            if self.direction == Qt.LayoutDirection.RightToLeft:
+                transform: QTransform = QTransform()
+                transform.translate(image.width(), 0)
+                transform.scale(-1, 1)
+                wordBox = transform.mapRect(wordBox)
+
+            p.setPen(Qt.darkGray)
+            p.drawRect(wordBox)
+            # p.drawLine(box.topLeft(), box.bottomRight())
+            # p.drawLine(box.bottomLeft(), box.topRight())
+
+            white = QColor(Qt.white)
+            white.setAlpha(int(w.probability*255))
+            p.setPen(white)
+            p.setFont(QFont('Noto Sans Arabic', 18))
+            p.drawText(wordBox, Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignHCenter, w.word)
+
+            p.setPen(Qt.black)
+            p.setFont(QFont('Courier New', 12, weight=900))
+            pc = f'{int(w.probability*100)}%'
+            p.drawText(wordBox, Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignHCenter, pc)
+
+        lt = 0
+        rt = image.width() - 1
+        top = 0
+        btm = image.height() - 1
+        imageBox = QRectF(QPointF(lt, top), QPointF(rt, btm))
+
+        align = Qt.AlignmentFlag.AlignVCenter | (Qt.AlignmentFlag.AlignRight
+                                                 if self.direction == Qt.LayoutDirection.RightToLeft
+                                                 else Qt.AlignmentFlag.AlignLeft)
+        p.setFont(QFont('Courier New', 18, weight=900))
+        bb = p.boundingRect(imageBox, align, label).marginsAdded(QMargins(6, 1, 6, 1))
+        if self.direction == Qt.LayoutDirection.RightToLeft:
+            bb.moveRight(imageBox.right())
+        else:
+            bb.moveLeft(imageBox.left())
+        p.setBrush(QBrush(QColor(Qt.yellow)))
+        p.drawRoundedRect(bb, 3.0, 3.0)
+
+        p.setPen(Qt.black)
+        p.drawText(bb, Qt.AlignmentFlag.AlignCenter, label)
+
+        p.end()
+
+        return image
+
+
+# ******************************************************************************
+if __name__ == '__main__':
+    import orjson as json
+    from pathlib import Path
+    from PySide6.QtGui import QColor, QGuiApplication
+    app = QGuiApplication()
+
+    tms = lambda x: int(x * 1000)
+    j = json.loads(Path(r"D:\tools\urdu-youtube\zia-mohyeddin\Dawood Rehber ｜ Zia Mohyeddin Ke Sath Aik Shaam Vol.24 [cHUQ1P2kb58].json").read_text(encoding='utf-8'))
+    ac = AudioClip.createAudioClip(Path(r"D:\tools\urdu-youtube\zia-mohyeddin\Dawood Rehber ｜ Zia Mohyeddin Ke Sath Aik Shaam Vol.24 [cHUQ1P2kb58].opus"))
+    ci = ClipImage(ac, widthPerSec=256, height=96, direction=Qt.LayoutDirection.RightToLeft, cmap='viridis')
+
+    for segment in [Segment.parse_obj(s) for s in j['segments'][:10]]:
+        img = ci.renderSpectrum(startTime=int(segment.start * 1000), endTime=int(segment.end * 1000))
+        img = ci.renderWords(image=img, label=f'#{segment.id}', words=segment.words)
+        img.save(f'segment{segment.id:002}.png')
