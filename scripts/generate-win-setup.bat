15e4e8b7e3737d38c7f9d98ee9b6aa083f3ccb07
roximn <roximn148@gmail.com>
2025-09-03 12:46:23 +0300

    Updated installer generation script
    * Added ui compilation step

--- scripts/generate-win-setup.bat
+++ scripts/generate-win-setup.bat
@@ -6,6 +6,7 @@ call make clean
 call make html
 cd ..
 
+python scripts/compileUI.py
 pyinstaller --clean --noconfirm rekhtanavees.app.spec
 
 %INNOSETUP_DIR%\ISCC.exe installer\rekhtanavees-installer.iss
