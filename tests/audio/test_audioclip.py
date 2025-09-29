8d787ff5af8027e486f2a731f2b4df12253a07a4
RoXimn <roximn@rixir.org>
2025-09-29 23:32:29 +0300

    Bumped up the copyright year

--- tests/audio/test_audioclip.py
+++ tests/audio/test_audioclip.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # ******************************************************************************
-# Copyright (c) 2022. All rights reserved.
+# Copyright (c) 2025. All rights reserved.
 #
 # This work is licensed under the Creative Commons Attribution 4.0 International License.
 # To view a copy of this license, visit # http://creativecommons.org/licenses/by/4.0/.
